19f6f76aebc0cd5f8f8cde6d14462e5d353508b2
doe-github <110170507+doe-github@users.noreply.github.com>
2023-10-27 00:51:11 +0200

    Update layout.tsx

--- dopd-app/app/layout.tsx
+++ dopd-app/app/layout.tsx
@@ -4,7 +4,8 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const bodyClass = inter.className + " flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-200"
+const bodyClass = inter.className + " bg-gray-100 dark:bg-gray-900 flex flex-col items-center justify-center h-screen text-gray-700 dark:text-gray-200"
+
 export const runtime = 'edge';
 
 export const metadata: Metadata = {
