9640976d2b0e59e1b2f109fd742a835baf5de036
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-10-27 00:31:35 +0200

    edit under construction page

--- dopd-app/app/layout.tsx
+++ dopd-app/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de-AT">
-      <body className={inter.className}>{children}</body>
+      <body className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-200">{children}</body>
     </html>
   )
 }
