9ff107086dd97bac63da396061318184dd1712f0
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-10-27 18:14:23 +0200

    add nextUI

--- dopd-app/app/layout.tsx
+++ dopd-app/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import {Providers} from "./providers";
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-const bodyClass = inter.className + " bg-gray-100 dark:bg-gray-900 flex flex-col items-center justify-center h-screen text-gray-700 dark:text-gray-200"
 
 export const runtime = 'edge';
 
@@ -19,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="de-AT">
-      <body className={bodyClass}>{children}</body>
+    <html lang="de-AT" className='dark'>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
