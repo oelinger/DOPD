4d93bdf62232c13a9319c49d5a5a204df311a4de
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-10-27 21:31:38 +0200

    try to get tailwind runing correctly

--- dopd-app/app/page.tsx
+++ dopd-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main className="font-arial flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-                <div className='text-4xl'>&#x1F6A7;</div>
+                <div className='text-8xl'>&#x1F6A7;</div>
                 <h1 className="mb-3 text-3xl font-semibold">Willkommen
                     bei D.O.P.D.</h1>
                 <p className="text-2xl">Diese Seite befindet sich derzeit im Aufbau. Bitte schauen Sie in Kürze wieder
