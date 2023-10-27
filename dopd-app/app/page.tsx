453f1636e277ea5dab3f6bc02c0203e32e7835e5
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-10-27 21:34:18 +0200

    try to get tailwind runing correctly

--- dopd-app/app/page.tsx
+++ dopd-app/app/page.tsx
@@ -3,7 +3,7 @@ export const runtime = 'edge';
 export default function Home() {
     return (
         <main className="font-arial flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+            <div className="z-10 max-w-8xl w-full items-center justify-between text-sm lg:flex">
                 <div className='text-8xl'>&#x1F6A7;</div>
                 <h1 className="mb-3 text-3xl font-semibold">Willkommen
                     bei D.O.P.D.</h1>
