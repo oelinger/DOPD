c3b5cbac086d443bc9d7a159791cc8e05d77efff
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-10-30 00:15:16 +0100

    add preview

--- dopd-app/app/components/Button/MailtoButton.tsx
+++ dopd-app/app/components/Button/MailtoButton.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import mailTo from "@/app/utils/mailTo";
-import { Button } from "@nextui-org/react";
+import {Button} from "@nextui-org/react";
 
-export default function MailtoButton(email: string) {
+export default function MailtoButton({email}: { email: { email: string } }) {
     return (
-        <Button onClick={() => mailTo(email)}></Button>
+        <Button onClick={() => mailTo(email.email)}>{email.email}</Button>
     )
 }
--- dopd-app/app/preview/page.tsx
+++ dopd-app/app/preview/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         <main className="font-arial flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-10xl w-full items-center justify-between text-sm lg:flex">
                 <h1 className="mb-3 text-3xl font-semibold">Hello</h1>
-                <MailtoButton email="office@oelinger.at"></MailtoButton>
+                <MailtoButton email={{email: "office@oelinger.at"}}></MailtoButton>
             </div>
         </main>
     )
