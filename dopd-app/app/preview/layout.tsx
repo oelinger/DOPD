9a912733b8caea2c64aa5862584f889c051d4b3e
Daniel Oelinger <daniel.oelinger@niceshops.com>
2023-10-30 11:39:32 +0100

    remove scrollbar on underconstruction page

--- dopd-app/app/preview/layout.tsx
+++ dopd-app/app/preview/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="de-AT" className='dark'>
+    <html lang="de-AT" className="dark overflow-hidden">
       <body>
           <main>
             <Providers>
