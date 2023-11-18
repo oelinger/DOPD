0ed5ed8dc61dc53a5fc5ed111e2011a4e23997b6
doe-github <110170507+doe-github@users.noreply.github.com>
2023-11-18 17:20:46 +0100

    Update page.tsx

--- dopd-app/app/page.tsx
+++ dopd-app/app/page.tsx
@@ -4,7 +4,7 @@ export const runtime = 'edge';
 
 export default function Home() {
     return (
-            <div className="font-arial flex flex-col items-center justify-center p-8 text-center">
+            <div style={{height: "100svh"}} className="font-arial flex flex-col items-center justify-center p-8 text-center">
                 <div className="z-10 max-w-2xl w-full items-center justify-between text-sm">
                     <div className='text-8xl mb-8'>&#x1F6A7;</div>
                     <h1 className="mb-3 text-3xl font-semibold">Willkommen
