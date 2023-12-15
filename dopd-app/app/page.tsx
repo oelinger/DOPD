eaae65ab84851af1ef92850a3e51e6309853530e
danieloelinger <33933682+danieloelinger@users.noreply.github.com>
2023-12-15 21:21:21 +0100

    add stylex

--- dopd-app/app/page.tsx
+++ dopd-app/app/page.tsx
@@ -1,22 +1,36 @@
 import MailtoButton from "@/app/components/Button/MailtoButton";
+import * as StyleX from '@stylexjs/stylex';
 
 export const runtime = 'edge';
 
+const style = StyleX.create({
+    container: {
+        fontFamily: 'Arial',
+        display: 'flex',
+        minHeight: '100svh',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '8px',
+        textAlign: 'center',
+    },
+});
+
 export default function Home() {
     return (
-            <div style={{height: "100svh"}} className="font-arial flex flex-col items-center justify-center p-8 text-center">
-                <div className="z-10 max-w-2xl w-full items-center justify-between text-sm">
-                    <div className='text-8xl mb-8'>&#x1F6A7;</div>
-                    <h1 className="mb-3 text-3xl font-semibold">Willkommen
-                        bei D.O.P.D.</h1>
-                    <p className="text-2xl">Diese Seite befindet sich derzeit im Aufbau. Bitte schaue in Kürze wieder
-                        vorbei!
-                        <br/>
-                        <br/>
-                        <MailtoButton className="text-sm md:text-md" email={"office@oelinger.at"}
-                                      label={"Schreibe eine E-Mail!"}/>
-                    </p>
-                </div>
+        <div {...StyleX.props(style.container)}>
+            <div className="z-10 max-w-2xl w-full items-center justify-between text-sm">
+                <div className='text-8xl mb-8'>&#x1F6A7;</div>
+                <h1 className="mb-3 text-3xl font-semibold">Willkommen
+                    bei D.O.P.D.</h1>
+                <p className="text-2xl">Diese Seite befindet sich derzeit im Aufbau. Bitte schaue in Kürze wieder
+                    vorbei!
+                    <br/>
+                    <br/>
+                    <MailtoButton className="text-sm md:text-md" email={"office@oelinger.at"}
+                                  label={"Schreibe eine E-Mail!"}/>
+                </p>
             </div>
+        </div>
     )
 }
