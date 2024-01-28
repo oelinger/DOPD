e4069504d9a7f02a9661f01e5a42f3b3d76b1a76
Daniel <daniel.oelinger@niceshops.com>
2024-01-28 23:25:35 +0100

    finish Imprint and Privacy

--- dopd-app/app/imprint/page.tsx
+++ dopd-app/app/imprint/page.tsx
@@ -1,4 +1,5 @@
 import * as StyleX from '@stylexjs/stylex';
+import Link from 'next/link';
 
 export const runtime = 'edge';
 
@@ -64,67 +65,56 @@ export default function Imprint() {
                     <div {...StyleX.props(style.content)}>
                         <h1 {...StyleX.props(style.headline)}>Impressum</h1>
                         <div {...StyleX.props(style.text)}>
-                            <p>Daniel Oelinger</p>
                             <p>
+                                Daniel Oelinger
+                                <br />
                                 Am Gaisberg 12
                                 <br />
                                 5310 Mondsee
                                 <br />
                                 Österreich
+                                <br />
+                                <br />
+                                Mail: <Link href={'mailto:office@oelinger.at'}>office@oelinger.at</Link>
                             </p>
-                            <p>
-                                Mail: <a href='mailto:office@oelinger.at'>office@oelinger.at</a>
-                            </p>
-                            <h2>
-                                <strong>Haftung:</strong>
-                            </h2>
-                            <p>
+                            <article>
+                                <h2>Haftung:</h2>
                                 Als Inhaber der Website bin ich bestrebt, die veröffentlichten Informationen stehts
                                 aktuell und richtig zu halten. Ich übernehme jedoch keine Verantwortung oder Haftung für
                                 die Aktualität und Richtigkeit sowie Vollständigkeit der Inhalte. Insofern kann ich für
                                 keinerlei Schäden, die sich aus der Nutzung des Inhalts ergeben können, haftbar gemacht
                                 werden.
-                            </p>
-                            <h2>
-                                <strong>Copyright:</strong>
-                            </h2>
-                            <p>
+                            </article>
+                            <article>
+                                <h2>Copyright:</h2>
                                 Sämtliche Texte, Grafiken, Bilder und Dokumente sind urheberrechtlich geschützt. Eine
                                 Verwendung jeglicher Art ist nur mit ausdrücklicher Genehmigung von D.O.P.D. zulässig.
-                            </p>
-                            <h2>
-                                <strong>Externe Links:</strong>
-                            </h2>
-                            <p>
+                            </article>
+                            <article>
+                                <h2>Externe Links:</h2>
                                 Diese Website enthält Links zu Seiten, die außerhalb dieses Servers im Internet liegen.
                                 Für den Inhalt und die Gestaltung der verlinkten Seiten sind ausschließlich deren
                                 Betreiber verantwortlich. D.O.P.D. macht sich den Inhalt der verlinkten Seiten nicht
                                 zueigen und distanziert sich hiermit ausdrücklich von etwaigen, gegen geltendes Recht
                                 verstoßenden Inhalten auf den verlinkten Seiten.
-                            </p>
-                            <h2>
-                                <strong>
-                                    <label>Anbieterkennzeichnung:</label>
-                                </strong>
-                            </h2>
-                            <p>
+                            </article>
+                            <article>
+                                <h2>Anbieterkennzeichnung:</h2>
                                 Die hier angeführten Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben
                                 zur Anbieterkennzeichnung der Internetpräsenz von Daniel Oelinger – Photogarphy &amp;
                                 Design (D.O.P.D.). Dieses Impressum gilt ebenso für die Auftritte bei Facebook und
                                 anderen sozialen Netzwerken.
-                            </p>
-                            <h2>
-                                <strong>Geschlechtsneutrale Formulierung:</strong>
-                            </h2>
-                            <p>
+                            </article>
+                            <article>
+                                <h2>Geschlechtsneutrale Formulierung:</h2>
                                 Lediglich zur Vereinfachung der Lesbarkeit erfolgt in den Texten dieser Website keine
                                 geschlechtsneutrale Differenzierung, sondern wird ausschließlich die männliche Form
                                 verwendet. Selbstverständlich ist ihre Ausrichtung in jedem Fall geschlechtsunabhängig.
-                            </p>
+                            </article>
                             <p>
                                 Bei Fragen nehmen Sie gerne Kontakt zu uns auf. Hier können Sie auch direkt Einsicht in
                                 unsere&nbsp;
-                                <a href='/privacy'>Datenschutzbestimmungen</a>
+                                <Link href={'/privacy'}>Datenschutzbestimmungen</Link>
                                 &nbsp;nehmen.
                             </p>
                         </div>
--- dopd-app/app/privacy/page.tsx
+++ dopd-app/app/privacy/page.tsx
@@ -64,60 +64,58 @@ export default function Imprint() {
                     <div {...StyleX.props(style.content)}>
                         <h1 {...StyleX.props(style.headline)}>Datenschutz</h1>
                         <div {...StyleX.props(style.text)}>
-                            <h2>
-                                <strong>Wer wir sind</strong>
-                            </h2>
-                            <p>
+                            <article>
+                                <h2>Wer wir sind</h2>
                                 D.O.P.D. steht für Daniel Oelinger Photography &amp; Design, die Adresse meiner Website
                                 ist: https://oelinger.at. Diese Seite dient als Portfolio und Galerie um einen Eindruck
                                 zu verschafften, was ich beruflich und künstlerisch geleistet habe.
-                            </p>
-                            <h2>
-                                <strong>Cookies</strong>
-                            </h2>
-                            <p>
-                                Diese Webseite verwendet nur unbedingt erforderliche Cookies die weder akzeptiert noch
-                                abgelehnt werden können. Sie gewährleisten lediglich die ordnungsgemäße funktionsweise
-                                dieser Website. Du hast jedoch die Möglichkeit, Cookies grundsätzlich im Browser zu
-                                deaktivieren.
-                            </p>
-                            <p>
-                                Verwendete Cookies:
-                                <br />
-                                Der Cookie „__cfduid“ wird von Cloudflare gesetzt und verwendet um vertrauenswürdigen
-                                Webverkehr zu identifizieren. Der Zweck dieses Cookies ist rein Funktional, es werden
-                                keine Daten gespeichert. Der Cookie hat eine Aufbewahrungsfrist von 30 Tagen.
-                            </p>
-                            <h2>
-                                <strong>Eingebettete Inhalte von anderen Websites</strong>
-                            </h2>
-                            <p>
-                                Beiträge auf dieser Website können eingebettete Inhalte beinhalten (z.&nbsp;B. Videos,
-                                Bilder, Beiträge etc.). Eingebettete Inhalte von anderen Websites verhalten sich exakt
-                                so, als ob der Besucher die andere Website besucht hätte.
-                            </p>
-                            <p>
-                                Diese Websites können Daten über dich sammeln, Cookies benutzen, zusätzliche
-                                Tracking-Dienste von Dritten einbetten und deine Interaktion mit diesem eingebetteten
-                                Inhalt aufzeichnen, inklusive deiner Interaktion mit dem eingebetteten Inhalt, falls du
-                                ein Konto hast und auf dieser Website angemeldet bist.
-                            </p>
-                            <h2>
-                                <strong>Mit wem wir deine Daten teilen</strong>
-                            </h2>
-                            <p>
-                                Grundsätzlich will D.O.P.D. keine Daten von dir sammeln oder teilen! Jedoch werden auf
-                                dieser Website Dienste verwendet um die Sicherheit und Schnelligkeit zu gewährleisten.
-                                Bitte entnehme deren Umgang mit Daten direkt aus deren Datenschutzerklärungen.
-                            </p>
-                            <h3>Cloudflare</h3>
-                            <p>
-                                Diese Website verwendet Cloudflare der Firma Cloudflare, Inc. (101 Townsend St., San
-                                Francisco, CA 94107, USA). Über nachfolgenden Link gelangst du zu ihrer
-                                Datenschutzerklärung.
-                                <br />
-                                https://www.cloudflare.com/de-de/privacypolicy/
-                            </p>
+                            </article>
+                            <article>
+                                <h2>Cookies</h2>
+                                <p>
+                                    Diese Webseite verwendet nur unbedingt erforderliche Cookies die weder akzeptiert
+                                    noch abgelehnt werden können. Sie gewährleisten lediglich die ordnungsgemäße
+                                    funktionsweise dieser Website. Du hast jedoch die Möglichkeit, Cookies grundsätzlich
+                                    im Browser zu deaktivieren.
+                                </p>
+                                <h3>Verwendete Cookies:</h3>
+                                <p>
+                                    Der Cookie „__cfduid“ wird von Cloudflare gesetzt und verwendet um
+                                    vertrauenswürdigen Webverkehr zu identifizieren. Der Zweck dieses Cookies ist rein
+                                    Funktional, es werden keine Daten gespeichert. Der Cookie hat eine
+                                    Aufbewahrungsfrist von 30 Tagen.
+                                </p>
+                            </article>
+                            <article>
+                                <h2>Eingebettete Inhalte von anderen Websites</h2>
+                                <p>
+                                    Beiträge auf dieser Website können eingebettete Inhalte beinhalten (z.&nbsp;B.
+                                    Videos, Bilder, Beiträge etc.). Eingebettete Inhalte von anderen Websites verhalten
+                                    sich exakt so, als ob der Besucher die andere Website besucht hätte.
+                                </p>
+                                <p>
+                                    Diese Websites können Daten über dich sammeln, Cookies benutzen, zusätzliche
+                                    Tracking-Dienste von Dritten einbetten und deine Interaktion mit diesem
+                                    eingebetteten Inhalt aufzeichnen, inklusive deiner Interaktion mit dem eingebetteten
+                                    Inhalt, falls du ein Konto hast und auf dieser Website angemeldet bist.
+                                </p>
+                            </article>
+                            <article>
+                                <h2>Mit wem wir deine Daten teilen</h2>
+                                <p>
+                                    Grundsätzlich will D.O.P.D. keine Daten von dir sammeln oder teilen! Jedoch werden
+                                    auf dieser Website Dienste verwendet um die Sicherheit und Schnelligkeit zu
+                                    gewährleisten. Bitte entnehme deren Umgang mit Daten direkt aus deren
+                                    Datenschutzerklärungen.
+                                </p>
+                                <h3>Cloudflare</h3>
+                                <p>
+                                    Diese Website verwendet Cloudflare der Firma Cloudflare, Inc. (101 Townsend St., San
+                                    Francisco, CA 94107, USA). Über nachfolgenden Link gelangst du zu ihrer
+                                    Datenschutzerklärung.
+                                </p>
+                                <p>https://www.cloudflare.com/de-de/privacypolicy/</p>
+                            </article>
                         </div>
                     </div>
                 </div>
