d43555c3536a56f23bbb4082991dd954d09b51df
Daniel.Oelinger <office@oelinger.at>
2024-08-09 17:59:56 +0200

    optimize DOPDImage.tsx

--- dopd-app/app/components/Image/DOPDImage.tsx
+++ dopd-app/app/components/Image/DOPDImage.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image';
 import React from 'react';
+import Image, { ImageProps } from '@nextui-org/image';
 
 export const runtime = 'edge';
 
-export default function DOPDImage(props: { src: string; alt: string; width: number; height: number }) {
+interface DOPDImageProps extends ImageProps {
+    src: string;
+    alt: string;
+    fallbackSrc: string;
+}
+
+export default function DOPDImage(props: DOPDImageProps) {
     // @ts-ignore
     return <Image src={props.src} alt={props.alt} width={props.width} height={props.height}></Image>;
 }
