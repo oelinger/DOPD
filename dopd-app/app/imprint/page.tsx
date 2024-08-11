83215693e4ff52017855597730bf4b2c4348fe0d
Daniel.Oelinger <office@oelinger.at>
2024-08-12 00:30:25 +0200

    improve imprint (new components)

--- dopd-app/app/imprint/page.tsx
+++ dopd-app/app/imprint/page.tsx
@@ -1,52 +1,15 @@
 import * as StyleX from '@stylexjs/stylex';
 import Link from 'next/link';
+import PageContainer from '@/app/components/PageContainer/PageContainer';
+import Inner from '@/app/components/PageContainer/Inner';
+import Content from '@/app/components/Content/Content';
 
 export const runtime = 'edge';
 
 const style = StyleX.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-    },
-    inner: {
-        zIndex: 10,
-        maxWidth: '64rem',
-        width: '100%',
-        margin: 'auto',
-        padding: '0 1rem',
-        '@media screen and (min-width: 1024px)': {
-            padding: '0 1.5rem',
-        },
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        fontSize: '3ch',
-    },
     headline: {
         fontSize: '3.5ch',
         fontWeight: '600',
-    },
-    contentWrap: {
-        display: 'grid',
-        gridTemplateColumns: '1fr',
-        '@media screen and (min-width: 768px)': {
-            gridTemplateColumns: '1fr 1fr',
-        },
-        gap: '1rem',
-        margin: '1rem 0',
-    },
-    content: {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '1rem',
-        margin: '1rem 0',
-        fontWeight: '400',
-        lineHeight: '1.5',
     },
     text: {
         display: 'flex',
@@ -59,68 +22,66 @@ const style = StyleX.create({
 
 export default function Imprint() {
     return (
-        <div>
-            <div {...StyleX.props(style.container)}>
-                <div {...StyleX.props(style.inner)}>
-                    <div {...StyleX.props(style.content)}>
-                        <h1 {...StyleX.props(style.headline)}>Impressum</h1>
-                        <div {...StyleX.props(style.text)}>
-                            <p>
-                                Daniel Oelinger
-                                <br />
-                                Am Gaisberg 12
-                                <br />
-                                5310 Mondsee
-                                <br />
-                                Österreich
-                                <br />
-                                <br />
-                                Mail: <Link href={'mailto:office@oelinger.at'}>office@oelinger.at</Link>
-                            </p>
-                            <article>
-                                <h2>Haftung:</h2>
-                                Als Inhaber der Website bin ich bestrebt, die veröffentlichten Informationen stehts
-                                aktuell und richtig zu halten. Ich übernehme jedoch keine Verantwortung oder Haftung für
-                                die Aktualität und Richtigkeit sowie Vollständigkeit der Inhalte. Insofern kann ich für
-                                keinerlei Schäden, die sich aus der Nutzung des Inhalts ergeben können, haftbar gemacht
-                                werden.
-                            </article>
-                            <article>
-                                <h2>Copyright:</h2>
-                                Sämtliche Texte, Grafiken, Bilder und Dokumente sind urheberrechtlich geschützt. Eine
-                                Verwendung jeglicher Art ist nur mit ausdrücklicher Genehmigung von D.O.P.D. zulässig.
-                            </article>
-                            <article>
-                                <h2>Externe Links:</h2>
-                                Diese Website enthält Links zu Seiten, die außerhalb dieses Servers im Internet liegen.
-                                Für den Inhalt und die Gestaltung der verlinkten Seiten sind ausschließlich deren
-                                Betreiber verantwortlich. D.O.P.D. macht sich den Inhalt der verlinkten Seiten nicht
-                                zueigen und distanziert sich hiermit ausdrücklich von etwaigen, gegen geltendes Recht
-                                verstoßenden Inhalten auf den verlinkten Seiten.
-                            </article>
-                            <article>
-                                <h2>Anbieterkennzeichnung:</h2>
-                                Die hier angeführten Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben
-                                zur Anbieterkennzeichnung der Internetpräsenz von Daniel Oelinger – Photogarphy &amp;
-                                Design (D.O.P.D.). Dieses Impressum gilt ebenso für die Auftritte bei Facebook und
-                                anderen sozialen Netzwerken.
-                            </article>
-                            <article>
-                                <h2>Geschlechtsneutrale Formulierung:</h2>
-                                Lediglich zur Vereinfachung der Lesbarkeit erfolgt in den Texten dieser Website keine
-                                geschlechtsneutrale Differenzierung, sondern wird ausschließlich die männliche Form
-                                verwendet. Selbstverständlich ist ihre Ausrichtung in jedem Fall geschlechtsunabhängig.
-                            </article>
-                            <p>
-                                Bei Fragen nehmen Sie gerne Kontakt zu uns auf. Hier können Sie auch direkt Einsicht in
-                                unsere&nbsp;
-                                <Link href={'/privacy'}>Daten&shy;schutz&shy;bestimmungen</Link>
-                                &nbsp;nehmen.
-                            </p>
-                        </div>
+        <PageContainer>
+            <Inner>
+                <Content>
+                    <h1 {...StyleX.props(style.headline)}>Impressum</h1>
+                    <div {...StyleX.props(style.text)}>
+                        <p>
+                            Daniel Oelinger
+                            <br />
+                            Am Gaisberg 12
+                            <br />
+                            5310 Mondsee
+                            <br />
+                            Österreich
+                            <br />
+                            <br />
+                            Mail: <Link href={'mailto:office@oelinger.at'}>office@oelinger.at</Link>
+                        </p>
+                        <article>
+                            <h2>Haftung:</h2>
+                            Als Inhaber der Website bin ich bestrebt, die veröffentlichten Informationen stehts aktuell
+                            und richtig zu halten. Ich übernehme jedoch keine Verantwortung oder Haftung für die
+                            Aktualität und Richtigkeit sowie Vollständigkeit der Inhalte. Insofern kann ich für
+                            keinerlei Schäden, die sich aus der Nutzung des Inhalts ergeben können, haftbar gemacht
+                            werden.
+                        </article>
+                        <article>
+                            <h2>Copyright:</h2>
+                            Sämtliche Texte, Grafiken, Bilder und Dokumente sind urheberrechtlich geschützt. Eine
+                            Verwendung jeglicher Art ist nur mit ausdrücklicher Genehmigung von D.O.P.D. zulässig.
+                        </article>
+                        <article>
+                            <h2>Externe Links:</h2>
+                            Diese Website enthält Links zu Seiten, die außerhalb dieses Servers im Internet liegen. Für
+                            den Inhalt und die Gestaltung der verlinkten Seiten sind ausschließlich deren Betreiber
+                            verantwortlich. D.O.P.D. macht sich den Inhalt der verlinkten Seiten nicht zueigen und
+                            distanziert sich hiermit ausdrücklich von etwaigen, gegen geltendes Recht verstoßenden
+                            Inhalten auf den verlinkten Seiten.
+                        </article>
+                        <article>
+                            <h2>Anbieterkennzeichnung:</h2>
+                            Die hier angeführten Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur
+                            Anbieterkennzeichnung der Internetpräsenz von Daniel Oelinger – Photogarphy &amp; Design
+                            (D.O.P.D.). Dieses Impressum gilt ebenso für die Auftritte bei Facebook und anderen sozialen
+                            Netzwerken.
+                        </article>
+                        <article>
+                            <h2>Geschlechtsneutrale Formulierung:</h2>
+                            Lediglich zur Vereinfachung der Lesbarkeit erfolgt in den Texten dieser Website keine
+                            geschlechtsneutrale Differenzierung, sondern wird ausschließlich die männliche Form
+                            verwendet. Selbstverständlich ist ihre Ausrichtung in jedem Fall geschlechtsunabhängig.
+                        </article>
+                        <p>
+                            Bei Fragen nehmen Sie gerne Kontakt zu uns auf. Hier können Sie auch direkt Einsicht in
+                            unsere&nbsp;
+                            <Link href={'/privacy'}>Daten&shy;schutz&shy;bestimmungen</Link>
+                            &nbsp;nehmen.
+                        </p>
                     </div>
-                </div>
-            </div>
-        </div>
+                </Content>
+            </Inner>
+        </PageContainer>
     );
 }
--- dopd-app/app/privacy/page.tsx
+++ dopd-app/app/privacy/page.tsx
@@ -1,51 +1,14 @@
 import * as StyleX from '@stylexjs/stylex';
+import PageContainer from '@/app/components/PageContainer/PageContainer';
+import Inner from '@/app/components/PageContainer/Inner';
+import Content from '@/app/components/Content/Content';
 
 export const runtime = 'edge';
 
 const style = StyleX.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-    },
-    inner: {
-        zIndex: 10,
-        maxWidth: '64rem',
-        width: '100%',
-        margin: 'auto',
-        padding: '0 1rem',
-        '@media screen and (min-width: 1024px)': {
-            padding: '0 1.5rem',
-        },
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        fontSize: '3ch',
-    },
     headline: {
         fontSize: '3.5ch',
         fontWeight: '600',
-    },
-    contentWrap: {
-        display: 'grid',
-        gridTemplateColumns: '1fr',
-        '@media screen and (min-width: 768px)': {
-            gridTemplateColumns: '1fr 1fr',
-        },
-        gap: '1rem',
-        margin: '1rem 0',
-    },
-    content: {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '1rem',
-        margin: '1rem 0',
-        fontWeight: '400',
-        lineHeight: '1.5',
     },
     text: {
         display: 'flex',
@@ -58,68 +21,64 @@ const style = StyleX.create({
 
 export default function Imprint() {
     return (
-        <div>
-            <div {...StyleX.props(style.container)}>
-                <div {...StyleX.props(style.inner)}>
-                    <div {...StyleX.props(style.content)}>
-                        <h1 {...StyleX.props(style.headline)}>Datenschutz</h1>
-                        <div {...StyleX.props(style.text)}>
-                            <article>
-                                <h2>Wer wir sind</h2>
-                                D.O.P.D. steht für Daniel Oelinger Photography &amp; Design, die Adresse meiner Website
-                                ist: https://oelinger.at. Diese Seite dient als Portfolio und Galerie um einen Eindruck
-                                zu verschafften, was ich beruflich und künstlerisch geleistet habe.
-                            </article>
-                            <article>
-                                <h2>Cookies</h2>
-                                <p>
-                                    Diese Webseite verwendet nur unbedingt erforderliche Cookies die weder akzeptiert
-                                    noch abgelehnt werden können. Sie gewährleisten lediglich die ordnungsgemäße
-                                    funktionsweise dieser Website. Du hast jedoch die Möglichkeit, Cookies grundsätzlich
-                                    im Browser zu deaktivieren.
-                                </p>
-                                <h3>Verwendete Cookies:</h3>
-                                <p>
-                                    Der Cookie „__cfduid“ wird von Cloudflare gesetzt und verwendet um
-                                    vertrauenswürdigen Webverkehr zu identifizieren. Der Zweck dieses Cookies ist rein
-                                    Funktional, es werden keine Daten gespeichert. Der Cookie hat eine
-                                    Aufbewahrungsfrist von 30 Tagen.
-                                </p>
-                            </article>
-                            <article>
-                                <h2>Eingebettete Inhalte von anderen Websites</h2>
-                                <p>
-                                    Beiträge auf dieser Website können eingebettete Inhalte beinhalten (z.&nbsp;B.
-                                    Videos, Bilder, Beiträge etc.). Eingebettete Inhalte von anderen Websites verhalten
-                                    sich exakt so, als ob der Besucher die andere Website besucht hätte.
-                                </p>
-                                <p>
-                                    Diese Websites können Daten über dich sammeln, Cookies benutzen, zusätzliche
-                                    Tracking-Dienste von Dritten einbetten und deine Interaktion mit diesem
-                                    eingebetteten Inhalt aufzeichnen, inklusive deiner Interaktion mit dem eingebetteten
-                                    Inhalt, falls du ein Konto hast und auf dieser Website angemeldet bist.
-                                </p>
-                            </article>
-                            <article>
-                                <h2>Mit wem wir deine Daten teilen</h2>
-                                <p>
-                                    Grundsätzlich will D.O.P.D. keine Daten von dir sammeln oder teilen! Jedoch werden
-                                    auf dieser Website Dienste verwendet um die Sicherheit und Schnelligkeit zu
-                                    gewährleisten. Bitte entnehme deren Umgang mit Daten direkt aus deren
-                                    Datenschutzerklärungen.
-                                </p>
-                                <h3>Cloudflare</h3>
-                                <p>
-                                    Diese Website verwendet Cloudflare der Firma Cloudflare, Inc. (101 Townsend St., San
-                                    Francisco, CA 94107, USA). Über nachfolgenden Link gelangst du zu ihrer
-                                    Datenschutzerklärung.
-                                </p>
-                                <p>https://www.cloudflare.com/de-de/privacypolicy/</p>
-                            </article>
-                        </div>
+        <PageContainer>
+            <Inner>
+                <Content>
+                    <h1 {...StyleX.props(style.headline)}>Datenschutz</h1>
+                    <div {...StyleX.props(style.text)}>
+                        <article>
+                            <h2>Wer wir sind</h2>
+                            D.O.P.D. steht für Daniel Oelinger Photography &amp; Design, die Adresse meiner Website ist:
+                            https://oelinger.at. Diese Seite dient als Portfolio und Galerie um einen Eindruck zu
+                            verschafften, was ich beruflich und künstlerisch geleistet habe.
+                        </article>
+                        <article>
+                            <h2>Cookies</h2>
+                            <p>
+                                Diese Webseite verwendet nur unbedingt erforderliche Cookies die weder akzeptiert noch
+                                abgelehnt werden können. Sie gewährleisten lediglich die ordnungsgemäße funktionsweise
+                                dieser Website. Du hast jedoch die Möglichkeit, Cookies grundsätzlich im Browser zu
+                                deaktivieren.
+                            </p>
+                            <h3>Verwendete Cookies:</h3>
+                            <p>
+                                Der Cookie „__cfduid“ wird von Cloudflare gesetzt und verwendet um vertrauenswürdigen
+                                Webverkehr zu identifizieren. Der Zweck dieses Cookies ist rein Funktional, es werden
+                                keine Daten gespeichert. Der Cookie hat eine Aufbewahrungsfrist von 30 Tagen.
+                            </p>
+                        </article>
+                        <article>
+                            <h2>Eingebettete Inhalte von anderen Websites</h2>
+                            <p>
+                                Beiträge auf dieser Website können eingebettete Inhalte beinhalten (z.&nbsp;B. Videos,
+                                Bilder, Beiträge etc.). Eingebettete Inhalte von anderen Websites verhalten sich exakt
+                                so, als ob der Besucher die andere Website besucht hätte.
+                            </p>
+                            <p>
+                                Diese Websites können Daten über dich sammeln, Cookies benutzen, zusätzliche
+                                Tracking-Dienste von Dritten einbetten und deine Interaktion mit diesem eingebetteten
+                                Inhalt aufzeichnen, inklusive deiner Interaktion mit dem eingebetteten Inhalt, falls du
+                                ein Konto hast und auf dieser Website angemeldet bist.
+                            </p>
+                        </article>
+                        <article>
+                            <h2>Mit wem wir deine Daten teilen</h2>
+                            <p>
+                                Grundsätzlich will D.O.P.D. keine Daten von dir sammeln oder teilen! Jedoch werden auf
+                                dieser Website Dienste verwendet um die Sicherheit und Schnelligkeit zu gewährleisten.
+                                Bitte entnehme deren Umgang mit Daten direkt aus deren Datenschutzerklärungen.
+                            </p>
+                            <h3>Cloudflare</h3>
+                            <p>
+                                Diese Website verwendet Cloudflare der Firma Cloudflare, Inc. (101 Townsend St., San
+                                Francisco, CA 94107, USA). Über nachfolgenden Link gelangst du zu ihrer
+                                Datenschutzerklärung.
+                            </p>
+                            <p>https://www.cloudflare.com/de-de/privacypolicy/</p>
+                        </article>
                     </div>
-                </div>
-            </div>
-        </div>
+                </Content>
+            </Inner>
+        </PageContainer>
     );
 }
